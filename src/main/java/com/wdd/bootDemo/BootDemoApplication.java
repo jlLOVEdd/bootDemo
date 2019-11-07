26d964fae3099dedf94b996b17c2e2c463623605
weidongdong <weidongdong@ctvit.com.cn>
2019-11-07 15:34:10 +0800

    测试回退commit

--- src/main/java/com/wdd/bootDemo/BootDemoApplication.java
+++ src/main/java/com/wdd/bootDemo/BootDemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootDemoApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(BootDemoApplication.class, args);
 	}
 
---
+++ src/main/java/com/wdd/bootDemo/action/testAction.java
@@ -0,0 +1,13 @@
+package com.wdd.bootDemo.action;
+
+/**
+ * @Description testAction
+ * @Author weidongdong
+ * @Date 2019/11/7 15:06
+ * @Version 1.0
+ */
+public class testAction {
+    public static void main(String[] args) {
+        System.out.println("----------git");
+    }
+}
